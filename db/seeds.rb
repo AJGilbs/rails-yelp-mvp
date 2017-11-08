4ac082552c6482f6d4753ff725fb3c6eb3583358
Andrew Gilbert <andrew-gilbert1@hotmail.co.uk>
2017-11-08 16:39:18 +0000

    changes made, navbar done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "L'Escargot",
+    address:      'Covent Garden',
+    category:     'french',
+    phone_number: '02088411746'
+  },
+   {
+    name:         'Happy Dumpling',
+    address:      'Brixton',
+    category:     'chinese',
+    phone_number: '02088675643'
+  },
+   {
+    name:         'Fujiyama',
+    address:      'Brixton',
+    category:     'japanese',
+    phone_number: '02088411746'
+  },
+   {
+    name:         'Belgo',
+    address:      'Camden',
+    category:     'belgian',
+    phone_number: '02082343451'
+  },
+   {
+    name:         'Franca Manca',
+    address:      'Earls Court',
+    category:     'italian',
+    phone_number: '02078838384'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
